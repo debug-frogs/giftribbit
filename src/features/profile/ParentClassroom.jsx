a3947032c4c721351bd072717f1d7e86924d8874
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-10-18 00:00:28 -0700

    Update ParentClassroom.jsx

--- src/features/profile/ParentClassroom.jsx
+++ src/features/profile/ParentClassroom.jsx
@@ -1,9 +1,17 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Grid, Typography} from "@mui/material";
 import {ProfileContext} from "../../pages/profile";
+import {DataStore} from "aws-amplify";
+import {Teacher} from "../../models";
 
 const ParentClassroom = () => {
-    const {Teacher} = useContext(ProfileContext)
+    const {teacherID} = useContext(ProfileContext)
+    const [teacher, setTeacher] = useState({})
+
+    useEffect(async () => {
+        const teacher = await DataStore.query(Teacher, teacherID);
+        setTeacher(teacher)
+    },[])
 
     return (
         <Grid
@@ -18,7 +26,7 @@ const ParentClassroom = () => {
             </Grid>
             <Grid item>
                 <Typography>
-                    {Teacher?.first_name}&nbsp;{Teacher?.last_name}
+                    {teacher?.first_name}&nbsp;{teacher?.last_name}
                 </Typography>
             </Grid>
         </Grid>
