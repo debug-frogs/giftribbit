24f152b53c7fb12b890a1f0389a9c96aad1328c4
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-10-26 20:23:02 -0700

    Update parent.js

--- src/pages/api/create/parent.js
+++ src/pages/api/create/parent.js
@@ -4,7 +4,6 @@ Amplify.configure({ ...config, ssr: true });
 
 import * as mutations from "../../../graphql/mutations";
 
-
 const createParent = async (API, input) => {
     return new Promise(async (resolve, reject) => {
         try {
