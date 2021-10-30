1044ae1b94ad10dfb5686950bb2bdc84f31a0896
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-10-30 14:51:21 -0700

    Update TeacherInformation.jsx

--- src/features/profile/teacher/TeacherInformation.jsx
+++ src/features/profile/teacher/TeacherInformation.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import {useContext} from 'react';
 import {Grid, Typography} from "@mui/material";
 import {ProfileContext} from "../../../pages/profile";
 
