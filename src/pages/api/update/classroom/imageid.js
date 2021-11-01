bb1b7f756a15eb9183b7ac7efa8a322ebc01d9f4
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-10-31 22:18:33 -0700

    Update imageid.js

--- src/pages/api/update/classroom/imageid.js
+++ src/pages/api/update/classroom/imageid.js
@@ -3,18 +3,30 @@ import config from "../../../../aws-exports.js";
 Amplify.configure({ ...config, ssr: true });
 
 import * as mutations from "../../../../graphql/mutations";
+import * as queries from "../../../../graphql/queries";
 
 export const updateClassroomImageID = async (API, input) => {
     return new Promise(async (resolve, reject) => {
         try {
-            /* Update Classroom data */
             const {classroomID, imageID} = input
+
+            const classroomData = await API.graphql({
+                query: queries.getClassroom,
+                variables:
+                    {
+                        id: classroomID
+                    }
+            });
+            const {_version} = classroomData.data.getClassroom
+
+            /* Update Classroom data */
             const updateClassroomData = await API.graphql({
                 query: mutations.updateClassroom,
                 variables: {
                     input: {
                         id: classroomID,
-                        imageID: imageID
+                        imageID: imageID,
+                        _version: _version
                     }
                 }
             })
