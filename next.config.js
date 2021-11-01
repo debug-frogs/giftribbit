dba4e2d3e760417a0151b9797eef6e0a64cf50a6
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-10-31 22:36:19 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-    webpack5: true,
+const withPlugins = require("next-compose-plugins");
+
+const nextConfig = {
+    images: {
+        domains: [
+            `giftribbit32638-feature.s3.us-west-1.amazonaws.com`,
+        ]
+    }
 }
+
+module.exports = withPlugins([], nextConfig)
