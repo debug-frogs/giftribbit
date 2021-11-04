609b0386bb753e429262f43948b806659b15a81c
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-11-04 00:50:12 -0700

    Update TeacherParentList.jsx

--- src/features/profile/teacher/TeacherParentList.jsx
+++ src/features/profile/teacher/TeacherParentList.jsx
@@ -1,10 +1,11 @@
 import {Fragment, useContext, useState} from 'react';
 import {ProfileContext} from "../../../pages/profile";
-import {Box, IconButton, List, ListItem, ListItemText, Modal} from "@mui/material";
+import {Box, Grid, Icon, IconButton, List, ListItem, ListItemText, Modal, Typography} from "@mui/material";
 import hash from 'object-hash'
 import {FaBackspace} from "react-icons/fa";
 import {TeacherParentsContext} from "./TeacherParents";
 import TeacherRemoveParent from "./TeacherRemoveParent";
+import {GiFrog} from 'react-icons/gi'
 
 
 const TeacherParentList = () => {
@@ -28,20 +29,47 @@ const TeacherParentList = () => {
         transform: 'translate(-50%, -50%)',
     };
 
-    return (
-        <List>
-            {Parents.map( (parent, i) =>
-                <Fragment key={hash(parent)+`${i}`}>
-                    <ListItem dense>
-                        <ListItemText
-                            primary={
-                                `${parent.first_name} ${parent.last_name}`
-                            }
-                            secondary={
-                                `parent of ${parent.child}`
-                            }
-                        />
-                        {removable &&
+    if (!Parents.length) {
+        return (
+            <Box py={3}>
+                <Grid
+                    container
+                    alignItems='center'
+                    justifyContent='center'
+                >
+                    <Grid item>
+                        <Typography
+                            variant='body2'
+                            display='inline'
+                        >
+                            Invite Parents to your Classroom..&nbsp;
+                        </Typography>
+                        <Icon
+                            fontSize='small'
+                            color='primary'
+                        >
+                            <GiFrog />
+                        </Icon>
+                    </Grid>
+                </Grid>
+            </Box>
+        )
+    }
+    else {
+        return (
+            <List>
+                {Parents.map( (parent, i) =>
+                    <Fragment key={hash(parent)+`${i}`}>
+                        <ListItem dense>
+                            <ListItemText
+                                primary={
+                                    `${parent.first_name} ${parent.last_name}`
+                                }
+                                secondary={
+                                    `parent of ${parent.child}`
+                                }
+                            />
+                            {removable &&
                             <IconButton
                                 edge="end"
                                 color='secondary'
@@ -49,21 +77,22 @@ const TeacherParentList = () => {
                             >
                                 <FaBackspace/>
                             </IconButton>
-                        }
-                    </ListItem>
-                    <Modal
-                        open={modalOpen}
-                        onClose={handleModalClose}
-                        style={{overflow: 'scroll',}}
-                    >
-                        <Box style={modalContentStyle}>
-                            <TeacherRemoveParent parent={parent} handleModalClose={handleModalClose}/>
-                        </Box>
-                    </Modal>
-                </Fragment>
-            )}
-        </List>
-    )
+                            }
+                        </ListItem>
+                        <Modal
+                            open={modalOpen}
+                            onClose={handleModalClose}
+                            style={{overflow: 'scroll',}}
+                        >
+                            <Box style={modalContentStyle}>
+                                <TeacherRemoveParent parent={parent} handleModalClose={handleModalClose}/>
+                            </Box>
+                        </Modal>
+                    </Fragment>
+                )}
+            </List>
+        )
+    }
 };
 
 export default TeacherParentList;
