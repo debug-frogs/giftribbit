3506c81aa717cf8c142c678743267fd7458c8a9a
Alexander Rose <alexander.rose.11@my.csun.edu>
2021-11-04 17:48:35 -0700

    Update Profile.jsx

--- src/features/profile/Profile.jsx
+++ src/features/profile/Profile.jsx
@@ -1,10 +1,11 @@
-import {useContext} from 'react';
+import {memo, useContext} from 'react';
 import {ProfileContext} from "../../pages/profile";
 import ParentProfile from "./parent/ParentProfile";
 import TeacherProfile from "./teacher/TeacherProfile";
 
-const Profile = () => {
+const Profile = memo(() => {
     const [profile] = useContext(ProfileContext)
+    console.log(profile)
     const {type} = profile
 
     switch (type){
@@ -19,6 +20,6 @@ const Profile = () => {
         default:
             return null
     }
-};
+})
 
 export default Profile
