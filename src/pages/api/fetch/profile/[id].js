393e3ba641e8ba0a427fb7a4f8736aa188989b2b
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-04 20:11:22 -0700

    Update [id].js

--- src/pages/api/fetch/profile/[id].js
+++ src/pages/api/fetch/profile/[id].js
@@ -2,7 +2,7 @@ import Amplify, {withSSRContext} from "aws-amplify";
 import config from "../../../../aws-exports.js";
 Amplify.configure({ ...config, ssr: true });
 
-import {getParent, getTeacher} from "../../../../graphql/queries";
+import {getClassroom, getParent, getTeacher} from "../../../../graphql/queries";
 
 
 export const fetchProfilePromise = (API, userID) => {
@@ -13,34 +13,31 @@ export const fetchProfilePromise = (API, userID) => {
                 query: getParent, variables: {id: userID}
             })
             if (!!getParentData.data.getParent) {
-                const {child, Donations, first_name, id, last_name, teacherID} = getParentData.data.getParent
+                const {child, Donations, first_name, id, last_name} = getParentData.data.getParent
 
                 const parentViewModel = {
                     child: child,
-                    Donations: Donations.items
+                    Classrooms: Donations.items
                         .filter(c => !c._deleted)
-                        .map(c => ({id: c.id})),
+                        .map( async c => {
+                            const getClassroomData = await API.graphql({
+                                query: getClassroom, variables: {id: c.classroomID}
+                            })
+                            const {id, imageID, Donations, Items, Teacher} = getClassroomData.data.getClassroom
+                            return ({
+                                id: id,
+                                Teacher: {
+                                    first_name: Teacher.first_name,
+                                    last_name: Teacher.last_name,
+                                    school: Teacher.school
+                                }})
+                        }),
                     first_name: first_name,
                     id: id,
                     last_name: last_name,
-                    Teacher: {},
                     type: "Parent"
                 }
 
-                if (teacherID) {
-                    const getTeacherData = await API.graphql({
-                        query: getTeacher,
-                        variables: { id: teacherID }
-                    })
-                    if (getTeacherData.data.getTeacher) {
-                        const teacher = getTeacherData.data.getTeacher
-                        parentViewModel.Teacher = {
-                            first_name: teacher.first_name,
-                            last_name: teacher.last_name,
-                            school: teacher.school
-                        }
-                    }
-                }
                 return resolve(parentViewModel)
             }
 
@@ -50,21 +47,13 @@ export const fetchProfilePromise = (API, userID) => {
                 variables: {id: userID}
             })
             if (!!getTeacherData.data.getTeacher) {
-                const {classroomID, first_name, id, last_name, Parents, school} = getTeacherData.data.getTeacher
+                const {classroomID, first_name, id, last_name, school} = getTeacherData.data.getTeacher
 
                 const teacherViewModel = {
                     classroomID: classroomID,
                     first_name: first_name,
                     id: id,
                     last_name: last_name,
-                    Parents: Parents.items.map( parent => (
-                        {
-                            child: parent.child,
-                            first_name: parent.first_name,
-                            id: parent.id,
-                            last_name: parent.last_name,
-                            donationID: parent.Donations.items.find(c => !c._deleted && c.classroomID === classroomID).id
-                        })),
                     school: school,
                     type: "Teacher"
                 }
