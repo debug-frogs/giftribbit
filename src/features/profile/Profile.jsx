68754ce9bcaec7bd2f068d30253a5224f664953c
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-16 23:23:12 -0800

    Create Profile.jsx

---
+++ src/features/profile/Profile.jsx
@@ -0,0 +1,24 @@
+import {memo, useContext} from 'react';
+import {ProfileContext} from "../../pages/profile";
+import ParentProfile from "./parent/ParentProfile";
+import TeacherProfile from "./teacher/TeacherProfile";
+
+const Profile = memo(() => {
+    const [profile] = useContext(ProfileContext)
+    const {type} = profile
+
+    switch (type){
+        case 'Parent':
+            return (
+                <ParentProfile />
+            )
+        case 'Teacher':
+            return (
+                <TeacherProfile />
+            )
+        default:
+            return null
+    }
+})
+
+export default Profile
