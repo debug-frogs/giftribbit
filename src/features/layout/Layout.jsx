324cb1ff2362155aa2f297e98ad7bb5a48b86caf
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-17 12:39:12 -0800

    Update Layout.jsx

--- src/features/layout/Layout.jsx
+++ src/features/layout/Layout.jsx
@@ -1,17 +1,30 @@
-import {Box, Container} from "@mui/material";
+import {Box, Container, GlobalStyles} from "@mui/material";
 import {useSelector} from "react-redux";
 import Navigation from "../navigation/Navigation";
 import Footer from "../footer/Footer";
 import LoadingIcon from "../loading/LoadingIcon";
 import {selectIsAuthPage} from "../auth/authSlice";
+import {Fragment} from "react";
+import {useTheme} from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 
 const Layout = (props) => {
     const isLoading = props.isLoading
     const isAuthPage = useSelector(selectIsAuthPage)
 
+    const theme = useTheme();
+    const isSm = useMediaQuery(theme.breakpoints.up('sm'));
+
     if (isAuthPage) {
-        return isLoading ? <LoadingIcon/> : props.children
+        return isLoading ?
+            <LoadingIcon/>
+            :
+            <Fragment>
+                <GlobalStyles styles={{body: {backgroundColor: "#efe5fd"}}}/>
+                {props.children}
+            </Fragment>
+
     }
     else {
         return (
@@ -29,7 +42,12 @@ const Layout = (props) => {
                         m={5}
                         minHeight='60vh'
                     >
-                        {isLoading ? <LoadingIcon/> : props.children}
+                        {isLoading ?
+                            <Fragment >
+                                {isSm && <GlobalStyles styles={{body: {backgroundColor: "#efe5fd"}}}/>}
+                                <LoadingIcon/>
+                            </Fragment>
+                            : props.children}
                     </Box>
                     <Footer/>
                 </Box>
