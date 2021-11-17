fec83a715832da02af282bea7325e6d5d62649c2
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-16 23:59:24 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
         domains: [
             `giftribbit32638-feature.s3.us-west-1.amazonaws.com`,
             `giftribbit05314-stag.s3.us-west-1.amazonaws.com`,
+            `giftribbit75025-production.s3.us-west-1.amazonaws.com`
         ]
     },
     webpack5: true
